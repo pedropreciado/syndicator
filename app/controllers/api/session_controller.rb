3a7d62207223d532362719ed80098ae014952d6a
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-02-06 14:09:58 -0800

    add root

--- app/controllers/api/session_controller.rb
+++ app/controllers/api/session_controller.rb
@@ -1,7 +1,25 @@
 class Api::SessionController < ApplicationController
   def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      login(@user)
+      render :show
+    else
+      render json: ["invalid username/password combination"], status: 401
+    end
   end
 
   def destroy
+    @user = current_user
+    if @user
+      logout
+      render '/api/users/show'
+    else
+      render json: ["no user"], status: 404
+    end
   end
+
 end

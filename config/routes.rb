5f6595ef9606fc86c8dd4ed45bfbf2503fbb6c67
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-02-05 18:59:48 -0800

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,55 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'session/create'
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:create]
+    resource :session, only: [:create, :destroy]
+    resources :events, only: [:create, :destroy, :index, :show, :update]
   end
-
-  namespace :api do
-    get 'session/destroy'
-  end
-
-  namespace :api do
-    get 'events/create'
-  end
-
-  namespace :api do
-    get 'events/destroy'
-  end
-
-  namespace :api do
-    get 'events/update'
-  end
-
-  namespace :api do
-    get 'events/show'
-  end
-
-  namespace :api do
-    get 'events/index'
-  end
-
-  namespace :api do
-    get 'users/create'
-  end
-
-  get 'session/create'
-
-  get 'session/destroy'
-
-  get 'events/create'
-
-  get 'events/destroy'
-
-  get 'events/update'
-
-  get 'events/index'
-
-  get 'events/show'
-
-  get 'users/create'
-
-  get 'users/destroy'
-
-  get 'users/show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

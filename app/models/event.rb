69b999a1ce364e42e6103df56c50a5c7969e89cc
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-02-05 18:24:25 -0800

    add user model auth

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  author_id   :integer
+#  description :string
+#  start_time  :time
+#  end_time    :time
+#  start_date  :date
+#  end_date    :date
+#  time_zone   :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Event < ApplicationRecord
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  author_id   :integer
+#  description :string
+#  start_time  :time
+#  end_time    :time
+#  start_date  :date
+#  end_date    :date
+#  time_zone   :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase

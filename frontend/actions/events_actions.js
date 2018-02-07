a75541fe3a76e68448d47b78292d00859b2040fb
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-02-06 18:10:23 -0800

    add events actions

---
+++ frontend/actions/events_actions.js
@@ -0,0 +1,22 @@
+import * as EventsAPIUtil from "../util/events_api_util";
+
+export const RECEIVE_ALL_EVENTS = "RECEIVE_ALL_EVENTS";
+export const RECEIVE_EVENT = "RECEIVE_EVENT";
+
+export const fetchEvents = (id) => dispatch => (
+  EventsAPIUtil.fetchEvents().then((events) => dispatch(receiveAllEvents(events)))
+);
+
+export const createEvent = (newEvent) => dispatch => (
+  EventsAPIUtil.createEvent().then((newEvent) => dispatch(receiveEvent(newEvent)))
+);
+
+const receiveAllEvents = events => ({
+  type: RECEIVE_ALL_EVENTS,
+  events
+});
+
+const receiveEvent = newEvent =. ({
+  type: RECEIVE_EVENT,
+  newEvent
+})
